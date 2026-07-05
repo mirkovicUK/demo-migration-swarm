c88094ece867a917c0c2cb766402b6b044b96af8
Aurora75 <mirkovic.uk@gmail.com>
2026-07-05 13:59:05 +0100

    Migrate js/index.ts (task e16b648f-1f02-4ca7-8b21-be3dd5a0e846)

---
+++ js/index.ts
@@ -0,0 +1,7 @@
+export * from './money';
+export * from './format';
+export * from './expense';
+export * from './split';
+export * from './balances';
+export * from './group';
+export * from './parse';
